ac597207a432d4a118625f0113459b69a60e2221
OwenEspinoza11 <110624917+OwenEspinoza11@users.noreply.github.com>
2023-01-20 11:52:19 -0600

    Add files via upload

---
+++ UtilidadCarro/CarroUtilidad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilidadCarro
+{
+    public class CarroUtilidad
+    {
+        double[,] utilidad = new double[3, 5];
+
+        public CarroUtilidad(double[,] utilidad)
+        {
+            this.utilidad = utilidad;
+        }
+
+        public void setUtilidad(double[,] utilidad)
+        {
+            this.utilidad = utilidad;
+        }
+
+    }
+}
---
+++ UtilidadCarro/Program.cs
@@ -0,0 +1,56 @@
+﻿using UtilidadCarro;
+public class CalcularUtilidad
+{
+    public static void Main(string[] args)
+    {
+        CarroUtilidad utilidadC = new CarroUtilidad();
+
+        double[,] uc = new double[3, 5];
+        int col, fila;
+        double ingreso,egreso,utilidadF;
+
+        do
+        {
+            Console.WriteLine("Ingresos");
+            ingreso = Convert.ToInt32(Console.ReadLine());
+
+        } while (ingreso <= 0 || ingreso > 10);
+
+        for (fila = 0; fila < 3; fila++)
+        {
+
+            do
+            {
+                Console.WriteLine("Precio del carro");
+                uc[fila, 0] = Convert.ToDouble(Console.ReadLine());
+                utilidadC.setUtilidad(uc);
+            } while (uc[fila, 0] <= 12000 || uc[fila, 0] > 65000);
+
+            do
+            {
+                Console.WriteLine("Egresos");
+                uc[fila, 0] = Convert.ToDouble(Console.ReadLine());
+                utilidadC.setUtilidad(uc);
+            } while (uc[fila, 1] <= 0 || uc[fila, 1] > ingreso);
+
+            uc[fila, 2] = uc[fila, 0] * uc[fila, 1];
+            utilidadF = ingreso - uc[fila,2];
+
+        }
+
+        for (fila = 0; fila < ingreso; fila++)
+        {
+            for (col = 0; col < 5; col++)
+            {
+                Console.WriteLine("\n" + uc[fila, col]);
+            }
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine("La utilidad total es: " + utilidadF);
+
+    }
+
+
+}
