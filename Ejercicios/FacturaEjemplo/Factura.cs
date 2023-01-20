c9a4381fd7768b8f7e18c161f5ff0d6a2eea569c
OwenEspinoza11 <110624917+OwenEspinoza11@users.noreply.github.com>
2023-01-20 10:50:12 -0600

    Add files via upload

---
+++ Ejercicios/FacturaEjemplo/Factura.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturaEjemplo
+{
+    public class Factura
+    {
+        double[,] Fact = new double[10, 3];
+
+        public Factura() 
+        { 
+        
+        }
+
+        public Factura(double [,] Fact)
+        {
+            this.Fact = Fact;
+        }
+
+        public void setFact(double [,]Fact)
+        {
+            this.Fact=Fact;
+        }
+    }
+}
---
+++ Ejercicios/FacturaEjemplo/Program.cs
@@ -0,0 +1,62 @@
+﻿using FacturaEjemplo;
+public class SimFactura
+{
+    public static void Main(string[] args)
+    {
+        Factura f = new Factura();
+
+        double[,] fa = new double[10,3];
+        int cp, col, fila;
+        double stf = 0;
+        double tf;
+
+
+        do
+        {
+            Console.WriteLine("Cantidad de producto a gestionar");
+            cp = Convert.ToInt32(Console.ReadLine());
+
+        } while (cp <= 0 || cp > 10);
+
+        for (fila = 0; fila < 10; fila++)
+        {   
+
+            do
+            {
+                Console.WriteLine("Cantidad por producto");
+                fa[fila,0] = Convert.ToDouble(Console.ReadLine());
+                f.setFact(fa);
+            } while (fa[fila,0] <= 0 || fa[fila,0] > 3);
+
+            do
+            {
+                Console.WriteLine("Precio por producto");
+                fa[fila,0] = Convert.ToDouble(Console.ReadLine());
+                f.setFact(fa);
+            } while (fa[fila,0] <= 0);
+
+            fa[fila,2] = fa[fila,0] * fa[fila,1];
+            stf = stf + fa[fila,2];
+        }
+
+        tf = stf * 1.15;
+
+        for (fila = 0; fila < cp; fila++)
+        {
+            for (col = 0; col < 3; col++)
+            {
+                Console.WriteLine("\n" + fa[fila,col]);
+            }
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine("El subtotal de la factura es: " + stf);
+        Console.WriteLine("El total de la factura es: " + tf);
+
+
+    }
+
+}
+
+
